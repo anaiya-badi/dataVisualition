2a6a0e9985c0c6bdfcbab2f9f4fe9b149e153aa5
anaiya-badi <160623428+anaiya-badi@users.noreply.github.com>
2024-04-20 14:01:30 -0400

    ms 1 sans images

--- server.py
+++ server.py
@@ -14,8 +14,18 @@ def index():
     data = json.load(f)
     f.close()
 
+    #dropdown years
+    attacks = list(data.keys())
+    years = []
+
+    for attack in attacks:
+        if data[attack]["year"] not in years:
+            years.append(data[attack]["year"])
+
+    years.sort()
+
     
-    return render_template('index.html')
+    return render_template('index.html', years = years)
 
 @app.route('/micro')
 def year():
@@ -24,11 +34,37 @@ def year():
     data = json.load(f)
     f.close()
 
-    
-    return render_template('micro.html')
+    requested_year = request.args.get('year')
+
+    #dropdown years
+    attacks = list(data.keys())
+    years = []
+
+    for attack in attacks:
+        if data[attack]["year"] not in years:
+            years.append(data[attack]["year"])
+
+    years.sort()
+
+    return render_template('micro.html', years = years, requested_year = requested_year)
 
 @app.route('/about')
 def about():
-    return render_template('about.html')
+    #load a current view of the data
+    f = open("data/suicide_bombings.json", "r")
+    data = json.load(f)
+    f.close()
+
+    #dropdown years
+    attacks = list(data.keys())
+    years = []
+
+    for attack in attacks:
+        if data[attack]["year"] not in years:
+            years.append(data[attack]["year"])
+
+    years.sort()
+
+    return render_template('about.html', years = years)
 
 app.run(debug=True)
